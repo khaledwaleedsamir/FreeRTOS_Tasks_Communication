d1824cec37927fd672d23f94b9407a107d41b513
Khaled Waleed Samir Metwally <khaledwaleed42069@gmail.com>
2023-06-16 17:25:15 +0300

    Initial Commit of the demo file.

---
+++ Debug/FreeRTOS Source/event_groups.d
@@ -0,0 +1,28 @@
+FreeRTOS Source/event_groups.o: ../FreeRTOS\ Source/event_groups.c \
+ C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/FreeRTOS.h \
+ ../include/FreeRTOSConfig.h ../system/include/cmsis/system_stm32f4xx.h \
+ C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/projdefs.h \
+ C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/portable.h \
+ C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/deprecated_definitions.h \
+ C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\portable\GCC\ARM_CM4F/portmacro.h \
+ C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/mpu_wrappers.h \
+ C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/task.h \
+ C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/list.h \
+ C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/timers.h \
+ C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/task.h \
+ C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/event_groups.h \
+ C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/timers.h
+C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/FreeRTOS.h:
+../include/FreeRTOSConfig.h:
+../system/include/cmsis/system_stm32f4xx.h:
+C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/projdefs.h:
+C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/portable.h:
+C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/deprecated_definitions.h:
+C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\portable\GCC\ARM_CM4F/portmacro.h:
+C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/mpu_wrappers.h:
+C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/task.h:
+C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/list.h:
+C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/timers.h:
+C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/task.h:
+C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/event_groups.h:
+C:\Users\lenovo\Downloads\dd\Demo\FreeRTOS\ Source\include/timers.h:
---
+++ Debug/src/main.d
@@ -0,0 +1,29 @@
+src/main.o: ../src/main.c ../system/include/diag/trace.h \
+ C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/FreeRTOS.h \
+ ../include/FreeRTOSConfig.h ../system/include/cmsis/system_stm32f4xx.h \
+ C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/projdefs.h \
+ C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/portable.h \
+ C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/deprecated_definitions.h \
+ C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\portable\GCC\ARM_CM4F/portmacro.h \
+ C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/mpu_wrappers.h \
+ C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/task.h \
+ C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/list.h \
+ C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/queue.h \
+ C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/timers.h \
+ C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/task.h \
+ ../include/led.h
+../system/include/diag/trace.h:
+C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/FreeRTOS.h:
+../include/FreeRTOSConfig.h:
+../system/include/cmsis/system_stm32f4xx.h:
+C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/projdefs.h:
+C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/portable.h:
+C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/deprecated_definitions.h:
+C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\portable\GCC\ARM_CM4F/portmacro.h:
+C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/mpu_wrappers.h:
+C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/task.h:
+C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/list.h:
+C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/queue.h:
+C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/timers.h:
+C:\Users\assas\Desktop\FreeRTOS_Tasks_Communication\FreeRTOS_Tasks_Communication\FreeRTOS\ Source\include/task.h:
+../include/led.h:
